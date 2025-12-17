8014b3337845bfcf7be567e6e31e0378edbcb7cd
madsuleiman <ahmadsuleimanbusiness@gmail.com>
2025-12-16 22:35:40 -0800

    fixed sonner to bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default async function RootLayout({
               {children}
             </Suspense>
           </div>
-          <Toaster position="top-right" />
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -10,20 +10,16 @@ import {
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
-import { useIsMobile } from "@/hooks/use-mobile";
-
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
-  const isMobile = useIsMobile();
 
-  const { position = "top-right", ...rest } = props;
-  const resolvedPosition = isMobile ? "bottom-center" : position;
+  const { position = "bottom-left", ...rest } = props;
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      position={resolvedPosition}
+      position={position}
       icons={{
         success: <CircleCheckIcon className="size-4" />,
         info: <InfoIcon className="size-4" />,
